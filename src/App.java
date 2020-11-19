9b1264a85251e6ca340914b92b2448da865eb093
d3vkk <donaldkk22@yahoo.com>
2020-11-19 13:23:01 +0300

    refactor: Identify each output

--- src/App.java
+++ src/App.java
@@ -10,25 +10,25 @@ public class App {
 
         // Creating an object
         Student firstStudent = new Student("COM/1000/88", "Makoyo", "dadsa", 32);
-        System.out.println(firstStudent.age);
+        System.out.println("First Student Age: " + firstStudent.age);
 
         // Calling a getter
         firstStudentRegNo = firstStudent.getRegNo();
-        System.out.println(firstStudentRegNo);
+        System.out.println("First Student Reg No: " + firstStudentRegNo);
 
         // Calling a setter
         firstStudent.setName("Mariba");
-        System.out.println(firstStudent.name);
+        System.out.println("First Student Name: " + firstStudent.name);
 
         // Class variable can be referenced directly
         String firstStudentGender = Student.gender;
-        System.out.println(firstStudentGender);
+        System.out.println("First Student Gender: " + firstStudentGender);
 
         // When a String is empty it'll show null
         // When a int is empty it'll show 0
 
         Student secondStudent = new Student("COM/900/75", "Mbuyu", "Med", 33);
-        System.out.println(secondStudent.calculateDOB());
-        System.out.println(secondStudent.calculateDOB(2020));
+        System.out.println("Year of Birth: " + secondStudent.calculateDOB());
+        System.out.println("Year of Birth: " + secondStudent.calculateDOB(2020));
     }
 }
